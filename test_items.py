370ee0acc2b328cd5437b047dc420d90f786fe76
Sergey Vyakhirev <s.vyakhirev@gmail.com>
2021-02-04 21:17:29 +0300

    changes for Lesson 3.6 Step 9

--- test_items.py
+++ test_items.py
@@ -1,6 +1,8 @@
-link = "http://selenium1py.pythonanywhere.com/"
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 
-def test_guest_should_see_login_link(browser):
+def test_add_to_basket_is_available(browser):
     browser.get(link)
-    browser.find_element_by_css_selector("#login_link")
+    button = browser.find_element_by_css_selector(".btn-add-to-basket")
+    
+    assert button, "Add to basket is unavailable"
